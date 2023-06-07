a316ad360ea3d0fb4ab073372b5b2ef7444dfc6d
Segacnd <segabelka@yandex.ru>
2023-06-07 11:11:24 +0300

    feat: add axios

---
+++ src/components/exchanger/exchange.ts
@@ -0,0 +1,27 @@
+//  Convert request from to
+// const url = `https://api.apilayer.com/fixer/latest?base=${from}&symbols=${to}`;
+// All avelible currency
+// const url = 'https://api.apilayer.com/fixer/symbols';
+
+//
+// const apiKey = 'zYaZxBM3aLZGaUEPKbsQHERLz2kIsO2d';
+// const config = {
+//   headers: {
+//     apikey: apiKey,
+//   },
+// };
+
+// axios.get(url, config);
+
+// const apiKey = 'zYaZxBM3aLZGaUEPKbsQHERLz2kIsO2d';
+// const url = 'https://api.apilayer.com/fixer/symbols';
+// const config = {
+//   headers: {
+//     apikey: apiKey,
+//   },
+// };
+
+// axios
+//   .get(url, config)
+//   .then((res) => console.log(res.data))
+//   .catch((err) => console.log(err));
