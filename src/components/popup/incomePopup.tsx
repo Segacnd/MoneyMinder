8ae5417e7a688b2ca313a6a0b0080a7747171932
Segacnd <segabelka@yandex.ru>
2023-06-09 21:57:25 +0300

    fix: fix type errors

--- src/components/popup/incomePopup.tsx
+++ src/components/popup/incomePopup.tsx
@@ -2,7 +2,6 @@ import { motion } from 'framer-motion';
 import * as React from 'react';
 
 import Button from '@/components/buttons/Button';
-import TextInput from '@/components/inputs/textInput';
 
 import CloseIcon from '@/assets/icons/close';
 
@@ -14,26 +13,6 @@ export default function IncomePopupPage() {
           <CloseIcon />
         </button>
         <h2 className='text-center'>Add income</h2>
-        <TextInput
-          placeholder='Enter your job name'
-          name='jobname'
-          type='text'
-        />
-        <TextInput
-          placeholder='Enter your currency'
-          name='currency'
-          type='text'
-        />
-        <TextInput
-          placeholder='Enter your income date'
-          name='date'
-          type='text'
-        />
-        <TextInput
-          placeholder='Enter your ammount'
-          name='ammount'
-          type='number'
-        />
         <Button className='mt-auto flex w-full items-center justify-center'>
           Create
         </Button>
